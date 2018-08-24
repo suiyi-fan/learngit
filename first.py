e892418252db4422981f7ebfbaf3e2cb876969b0
suiyi <1300682585@qq.com>
2018-08-24 02:36:57 -0400

    mysql study by py

---
+++ first.py
@@ -0,0 +1,31 @@
+#!/user/bin/env python
+#coding:utf-8
+
+import mysql.connector
+
+# 连接
+con = mysql.connector.connect(host='66.98.113.68',user='root',password='suiyi',database='database_test')
+
+print(con.connection_id)
+
+cursor = con.cursor()
+
+# insert1 = ("insert into stu""(id,name,age)"" values (2,'Tom',20)")
+# cursor.execute(insert1)
+
+# update1 = "update stu set age=18 where id=2"
+# cursor.execute(update1)
+
+delete1 = "delete from stu where id=2"
+cursor.execute(delete1)
+
+
+con.commit()
+
+cursor.close()
+con.close()
+
+
+# ("INSERT INTO employees "
+#  "(first_name, last_name, hire_date, gender, birth_date) "
+#  "VALUES (%s, %s, %s, %s, %s)")
