9fe6a849b1aafb7b6e7527824cdebfa48494a457
suiyi <1300682585@qq.com>
2018-08-29 03:31:20 -0400

    Select Ready

--- mypw/select.py
+++ mypw/select.py
@@ -6,13 +6,14 @@ import sys
 
 
 # 接收终端参数
-#cli = sys.argv[1]
-
+cli = sys.argv[1]
+print('cli=='+cli)
 # 查询方法
 def selectData(cursor):
-	sql_select_data = " select acc,pw from mypw where id = %s "#+str(sys.argv[1])
+	sql_select_data = " select acc,pw from mypw where client = '"+str(sys.argv[1])+"'"
 	try:
-		cursor.execute(sql_select_data,(sys.argv[1]))
+		cursor.execute(sql_select_data)
+		print('sql_select_data=='+sql_select_data)
 		for (acc,pw) in cursor:
 			print('acc=',acc,'pw=',pw)
 	except mysql.connector.Error as e:
@@ -25,11 +26,13 @@ con = mysql.connector.connect(host='66.98.113.68',user='root',password='suiyi',d
 # 创建游标
 cursor = con.cursor()
 
+# sql_select_data = "select acc,pw from mypw where client = tx"#+cli #+ sys.argv[1]
+# cursor.execute(sql_select_data)
 # 执行查询
 selectData(cursor)
 
 # commit
-# con.commit()
+#con.commit()
 
 # 关闭游标
 cursor.close()
